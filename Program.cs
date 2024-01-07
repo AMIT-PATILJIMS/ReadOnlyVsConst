ac90f39d40f9115625539c842ff12a0a65e0004c
AMIT-PATILJIMS <ap1079997@gmail.com>
2024-01-07 12:49:53 +0530

    Changes: 1. Second difference. 2. We can declare/initialize const variable in methods. 3. We are not allowed t use readonly inside method.

--- Program.cs
+++ Program.cs
@@ -4,23 +4,19 @@ namespace ReadOnlyVsConst
 {
     public class Program
     {
-        /*
-         * Const has the same value throuout the lifetime of the application.
-         * We are not allowed to change the value of x.
-         */
-        const int x = 5;
-
-        /*
-         * readonly is also a constant.
-         * But we can change the value of readonly with constructor.
-         */
-
-        readonly int y = 10;
         static void Main(string[] args)
         {
-            Program obj = new Program();
+            /*
+             * We can declare/initialize const variable in methods.
+             */
+            const int x = 5;
+
+            /*
+             * We are not allowed t use readonly inside method. The below statement is giving error.
+             */
+            readonly int y = 10;
+
             Console.WriteLine(x);
-            Console.WriteLine(obj.y);
         }
     }
 }
